f1318ff660cd131bbc38a45b2a30363b43ffee15
Thiago Senna <trsenna@gmail.com>
2021-08-22 18:17:00 -0300

    added remove and retrieve capabilities to the customers repository

--- packages/customers/src/Domain/Model/CustomerRepository.php
+++ packages/customers/src/Domain/Model/CustomerRepository.php
@@ -2,7 +2,11 @@
 
 namespace Clean\Customers\Domain\Model;
 
+use Clean\Foundation\IdentityInterface;
+
 interface CustomerRepository
 {
+    public function ofIdentity(IdentityInterface $identity): Customer;
     public function add(Customer $customer): void;
+    public function remove(Customer $customer): void;
 }
--- packages/customers/src/Persistence/Eloquent/CustomerModelRepository.php
+++ packages/customers/src/Persistence/Eloquent/CustomerModelRepository.php
@@ -4,12 +4,24 @@ namespace Clean\Customers\Persistence\Eloquent;
 
 use Clean\Customers\Domain\Model\Customer;
 use Clean\Customers\Domain\Model\CustomerRepository;
+use Clean\Foundation\IdentityInterface;
 
 final class CustomerModelRepository implements CustomerRepository
 {
+    public function ofIdentity(IdentityInterface $identity): Customer
+    {
+        return CustomerModel::findOrFail($identity->value());
+    }
+
     public function add(Customer $customer): void
     {
         $eloquentModel = $customer->eloquent();
         $eloquentModel->save();
+    }
+
+    public function remove(Customer $customer): void
+    {
+        $eloquentModel = $customer->eloquent();
+        $eloquentModel->delete();
     }
 }
