33bf390e3b340d44bf07325e67cd20d33945cbe5
Thiago Senna <trsenna@gmail.com>
2021-08-24 23:02:37 -0300

    abstract identity added

---
+++ packages/foundation/src/Domain/Model/AbstractIdentity.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Clean\Foundation\Domain\Model;
+
+use Clean\Contracts\Foundation\Domain\Model\Identity;
+
+abstract class AbstractIdentity implements Identity
+{
+    private string $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function value(): string
+    {
+        return $this->value;
+    }
+
+    public function sameAs(Identity $identity): bool
+    {
+        return $identity->value() === $this->value;
+    }
+}
